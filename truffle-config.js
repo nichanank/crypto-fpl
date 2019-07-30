0c73f50b94efa725adb1e8cd85972663377988be
nichanank <n.kesonpat@gmail.com>
2019-07-30 15:05:40 +0700

    updated truffle-config for testnets

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
 const path = require('path')
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "a2855c7103a644d29e2864d9865bb72d";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, 'cryptofpl-client/src/contracts'),
@@ -12,12 +12,25 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+     gas: 6721975
     },
     rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/' + infuraKey),
       host: "localhost",
       port: 8545,
       network_id: 4,
-      gas: 4700000
+      gas: 5500000,
+      confirmations: 6,
+      timeoutBlocks: 200
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/' + infuraKey),
+      host: "localhost",
+      port: 8545,
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 6,
+      timeoutBlocks: 200
     }
   }
 }
